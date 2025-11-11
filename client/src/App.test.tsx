667987881d3d023e92dccca96cd880c510c25100
Tonia Gonzalez <toniagonzalez.design@gmail.com>
2025-11-11 12:30:11 -0500

    chore: Update client test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders canvassing app", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByTestId("header");
+  expect(header).toBeInTheDocument();
+});
+
+test("renders add contact button", () => {
+  render(<App />);
+  const addContact = screen.getByTestId("add-contact-button");
+  expect(addContact).toBeInTheDocument();
+});
+
+test("renders contact list", () => {
+  render(<App />);
+  const contactList = screen.getByTestId("contact-list");
+  expect(contactList).toBeInTheDocument();
 });
